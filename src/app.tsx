b3e55bf8d4691047e373f707847e92dc7d125220
hideyuki-hori <montecarlo1101@gmail.com>
2024-02-15 09:41:51 +0900

    feat: Add Info component

--- src/app.tsx
+++ src/app.tsx
@@ -1,6 +1,7 @@
 import { css } from 'panda/css'
 import { HeatMap } from './features/heat-map'
 import { MouseMovementSampler } from './features/mouse-movement-sampler'
+import { Info } from './features/info'
 
 export function App() {
   return (
@@ -11,9 +12,14 @@ export function App() {
       <div class={css({
         position: 'absolute',
         zIndex: 100,
-        color: 'white'
+        color: 'white',
+        width: '100%',
       })}>
         <MouseMovementSampler />
+        <Info class={css({
+          position: 'fixed',
+          bottom: '0',
+        })} />
       </div>
       <HeatMap />
     </main>
