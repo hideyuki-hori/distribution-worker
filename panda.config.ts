8047c31c20adc62e41518a45e841592773577b17
hideyuki-hori <montecarlo1101@gmail.com>
2024-02-23 11:14:14 +0900

    refactor

--- panda.config.ts
+++ panda.config.ts
@@ -1,20 +1,11 @@
-import { defineConfig } from "@pandacss/dev";
+import { defineConfig } from '@pandacss/dev'
 
 export default defineConfig({
-  // Whether to use css reset
   preflight: true,
-
-  // Where to look for your css declarations
-  include: ["./src/**/*.{js,jsx,ts,tsx}", "./pages/**/*.{js,jsx,ts,tsx}"],
-
-  // Files to exclude
+  include: ['./src/**/*.{ts,tsx}'],
   exclude: [],
-
-  // Useful for theme customization
   theme: {
     extend: {},
   },
-
-  // The output directory for your css system
-  outdir: "styled-system",
-});
+  outdir: 'styled-system',
+})
