50df14fa3d2e75485d3e8bb5473313fd977d5a32
hideyuki-hori <montecarlo1101@gmail.com>
2024-02-29 11:55:58 +0900

    perf: Reduce build size by optimizing imports and applying code-splitting

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,18 @@ import solid from 'vite-plugin-solid'
 import tsconfigPaths from 'vite-tsconfig-paths'
 
 export default defineConfig({
+  build: {
+    minify: true,
+    target: 'esnext',
+    rollupOptions: {
+      treeshake: true,
+      output: {
+        manualChunks(path) {
+          if (path.includes('node_modules')) return 'vendor'
+        },
+      },
+    },
+  },
   plugins: [
     solid(),
     tsconfigPaths(),
